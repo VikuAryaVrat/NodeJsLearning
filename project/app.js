c5046150adb2d326efd0b97e4217c4c8a4eebd2f
axelviku <axelice89@gmail.com>
2021-11-20 18:33:14 +0530

    Adding

--- project/app.js
+++ project/app.js
@@ -4,24 +4,22 @@ const app = express();
 const hbs = require('hbs');
 const port = process.env.PORT || 8000;
 
-const static_path = path.join(__dirname, "../public");
-const template_path = path.join(__dirname, "../templates/views");
-const partials_path = path.join(__dirname, "../templates/partials");
+const static_path = path.join(__dirname, "./public");
+const partials_path = path.join(__dirname, "/views/partials");
 
 app.set('view engine', 'hbs');
-app.set("views", template_path);
 hbs.registerPartials(partials_path);
 
 // console.log(static_path);
 app.use(express.static(static_path));
 
 app.get("", (req,res) =>{
-    res.render('index')
-})
+    res.render('index');
+});
 
 app.get("/about", (req,res) =>{
-    res.render('about')
-})
+    res.render('about');;
+});
 
 
 app.get("/weather", (req,res) =>{
